31556365b9e0addf709a57fc7982a8e92fafcb03
beneditorodrigo <rodrigodacunha7@gmail.com>
2022-10-09 01:03:31 -0300

    Adicionando acesso em memória com WebSecurityConfigurerAdapter

--- src/main/java/beneditorodrigo/springsecurity/controller/SegurancaController.java
+++ src/main/java/beneditorodrigo/springsecurity/controller/SegurancaController.java
@@ -1,13 +1,23 @@
 package beneditorodrigo.springsecurity.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SegurancaController {
-
-    @GetMapping("/")
-    public String index(){
-        return "Olá! Estou seguro com Spring Boot!";
+    @GetMapping
+    public String welcome(){
+        return "Bem-vindo a segurança com Spring Security!";
+    }
+    @GetMapping("/users")
+    @PreAuthorize("hasAnyRole('MANAGERS','USERS')")
+    public String users() {
+        return "Nível de usuário autorizado!";
+    }
+    @GetMapping("/managers")
+    @PreAuthorize("hasRole('MANAGERS')")
+    public String managers() {
+        return "Nível de administrador autorizado!";
     }
 }
